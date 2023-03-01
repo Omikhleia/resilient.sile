8c275f5749f6beddc9adb0fb36af236c30491eab
Omikhleia <didier.willis@gmail.com>
2023-03-01 03:17:45 +0100

    feat: [breaking] migrate epigraph package to styles v2 paradigm

--- packages/resilient/epigraph/init.lua
+++ packages/resilient/epigraph/init.lua
@@ -25,65 +25,23 @@ end
 
 function package.declareSettings (_)
   SILE.settings:declare({
-    parameter = "epigraph.beforeskipamount",
-    type = "vglue",
-    default = SILE.settings:get("plain.medskipamount"),
-    help = "Vertical offset before an epigraph (defaults to a medium skip)."
-  })
-
-  SILE.settings:declare({
-    parameter = "epigraph.afterskipamount",
-    type = "vglue",
-    default = SILE.settings:get("plain.bigskipamount"),
-    help = "Vertical offset after an epigraph (defaults to a big skip)."
-  })
-
-  SILE.settings:declare({
-    parameter = "epigraph.sourceskipamount",
-    type = "vglue",
-    default = SILE.settings:get("plain.smallskipamount"),
-    help = "Vertical offset betwen an epigraph text and its source (defaults to a small skip)."
-  })
-
-  SILE.settings:declare({
-    parameter = "epigraph.parindent",
-    type = "glue",
-    default = SILE.settings:get("document.parindent"),
-    help = "Paragraph identation in an epigraph (defaults to document paragraph indentation)."
-  })
-
-  SILE.settings:declare({
     parameter = "epigraph.width",
-    type = "length",
-    default = SILE.length("60%lw"),
+    type = "measurement",
+    default = SILE.measurement("60%lw"),
     help = "Width of an epigraph (defaults to 60% of the current line width)."
   })
 
   SILE.settings:declare({
     parameter = "epigraph.rule",
-    type = "length",
-    default = SILE.length("0"),
+    type = "measurement",
+    default = SILE.measurement(),
     help = "Thickness of the rule drawn below an epigraph text (defaults to 0, meaning no rule)."
   })
 
   SILE.settings:declare({
-    parameter = "epigraph.align",
-    type = "string",
-    default = "right",
-    help = "Position of an epigraph in the frame (right or left, defaults to right)."
-  })
-
-  SILE.settings:declare({
-    parameter = "epigraph.ragged",
-    type = "boolean",
-    default = false,
-    help = "Whether an epigraph is ragged (defaults to false)."
-  })
-
-  SILE.settings:declare({
     parameter = "epigraph.margin",
-    type = "length",
-    default = SILE.length("0"),
+    type = "measurement",
+    default = SILE.measurement(),
     help = "Margin (indent) for an epigraph (defaults to 0)."
   })
 end
@@ -91,32 +49,17 @@ end
 function package:registerCommands ()
   self:registerCommand("epigraph", function (options, content)
     SILE.settings:temporarily(function ()
-      local beforeskipamount =
-        options.beforeskipamount ~= nil and SU.cast("vglue", options.beforeskipamount)
-        or SILE.settings:get("epigraph.beforeskipamount")
-      local afterskipamount =
-        options.afterskipamount ~= nil and SU.cast("vglue", options.afterskipamount)
-        or SILE.settings:get("epigraph.afterskipamount")
-      local sourceskipamount =
-        options.sourceskipamount ~= nil and SU.cast("vglue", options.sourceskipamount)
-        or SILE.settings:get("epigraph.sourceskipamount")
       local parindent =
         options.parindent ~= nil and SU.cast("glue", options.parindent)
-        or SILE.settings:get("epigraph.parindent")
+        or SILE.settings:get("document.parindent")
       local width =
-        options.width ~= nil and SU.cast("length", options.width)
+        options.width ~= nil and SU.cast("measurement", options.width)
         or SILE.settings:get("epigraph.width")
       local rule =
-        options.rule ~= nil and SU.cast("length", options.rule)
+        options.rule ~= nil and SU.cast("measurement", options.rule)
         or SILE.settings:get("epigraph.rule")
-      local align =
-        options.align ~= nil and SU.cast("string", options.align)
-        or SILE.settings:get("epigraph.align")
-      local ragged =
-        options.ragged ~= nil and SU.cast("boolean", options.ragged)
-        or SILE.settings:get("epigraph.ragged")
       local margin =
-        options.margin ~= nil and SU.cast("length", options.margin)
+        options.margin ~= nil and SU.cast("measurement", options.margin)
         or SILE.settings:get("epigraph.margin")
 
       local framew = SILE.typesetter.frame:width()
@@ -124,49 +67,85 @@ function package:registerCommands ()
       local skip = framew - epigraphw - margin
       local source = extractFromTree(content, "source")
       SILE.typesetter:leaveHmode()
-      SILE.typesetter:pushVglue(beforeskipamount)
 
-      local l = ragged
-        and SILE.length(skip, 1e10) -- some real huge strech
-        or SILE.length({ length = skip })
-      local glue = SILE.nodefactory.glue({ width = l })
+      local sty = self:resolveStyle("epigraph")
+      local align = sty.paragraph and sty.paragraph.align or "right"
       if align == "left" then
-        SILE.settings:set("document.rskip", glue)
-        SILE.settings:set("document.lskip", margin)
+        SILE.settings:set("document.lskip", SILE.nodefactory.glue(margin))
+        SILE.settings:set("document.rskip", SILE.nodefactory.glue(skip))
+      elseif align == "right" then
+        SILE.settings:set("document.lskip", SILE.nodefactory.glue(skip))
+        SILE.settings:set("document.rskip", SILE.nodefactory.glue(margin))
       else
-        SILE.settings:set("document.lskip", glue)
-        SILE.settings:set("document.rskip", margin)
+        -- undocumented because ugly typographically, IMHO
+        SILE.settings:set("document.lskip", SILE.nodefactory.glue((skip + margin) / 2))
+        SILE.settings:set("document.lskip", SILE.nodefactory.glue((skip + margin) / 2))
       end
 
       SILE.settings:set("document.parindent", parindent)
-      SILE.call("style:apply", { name = "epigraph" }, function()
+      SILE.call("style:apply:paragraph", { name = "epigraph-text" }, function()
         SILE.process(content)
         if rule:tonumber() ~= 0 then
           SILE.typesetter:leaveHmode()
           SILE.call("noindent")
           SILE.call("raise", { height = "0.5ex" }, function ()
-            SILE.call("hrule", {width = epigraphw, height = rule })
+            -- HACK. Oh my. When left-aligned, the rule is seen as longer than The
+            -- line and a new line is inserted. Tweaking it by 0.05pt seems to avoid
+            -- it. Rounding issue somewhere? I feel tired.
+            SILE.call("hrule", {width = epigraphw - 0.05, height = rule })
           end)
         end
         if source then
           SILE.typesetter:leaveHmode(1)
           if rule:tonumber() == 0 then
-            SILE.typesetter:pushVglue(sourceskipamount)
+            SILE.call("style:apply:paragraph", { name = "epigraph-source-norule" }, source)
+          else
+            SILE.call("style:apply:paragraph", { name = "epigraph-source-rule" }, source)
           end
-          SILE.call("style:apply", { name = "epigraph-source" }, function()
-            SILE.call("raggedleft", {}, source)
-          end)
         end
       end)
       SILE.typesetter:leaveHmode()
-      SILE.typesetter:pushVglue(afterskipamount)
     end)
   end, "Displays an epigraph.")
 end
 
 function package:registerStyles ()
-  self:registerStyle("epigraph", {}, { font = { size = "0.9em" } })
-  self:registerStyle("epigraph-source", {}, { font = { style="italic" } })
+  self:registerStyle("epigraph", {}, {
+    font = {
+      size = "0.9em"
+    },
+    paragraph = {
+      before = { skip = "medskip" },
+      after = { skip = "bigskip" }
+    }
+  })
+
+  self:registerStyle("epigraph-text", { inherit = "epigraph"}, {
+    paragraph = {
+      align = "justify",
+    }
+  })
+
+  self:registerStyle("epigraph-source", {}, {
+    -- Saner default without the italic we had in v1.
+    -- font = {
+    --   style = "italic"
+    -- },
+    paragraph = {
+      align = "right",
+      before = {
+        vbreak = false,
+        indent = false -- by default, we probablydon't expect a paragraph indent here.
+      }
+    }
+  })
+  self:registerStyle("epigraph-source-norule", { inherit = "epigraph-source" }, {
+    paragraph = {
+      before = { skip = "smallskip" }
+    }
+  })
+  self:registerStyle("epigraph-source-rule", { inherit = "epigraph-source" }, {
+  })
 end
 
 package.documentation = [[\begin{document}
@@ -182,7 +161,7 @@ very loosely inspired from the LaTeX \code{epigraph} package.}
 The \autodoc:environment{epigraph} environment typesets an epigraph using the provided text.
 An optional source (author, book name, etc.) can also be defined, with
 the \autodoc:command[check=false]{\source} command in the text block.
-By default the epigraph is placed at the right hand side of the text block,
+With the default styling, the epigraph is placed at the right hand side of the text block,
 and the source is typeset at the bottom right of the block.
 
 \begin{epigraph}
@@ -190,18 +169,10 @@ and the source is typeset at the bottom right of the block.
 \source{\randomsource}
 \end{epigraph}
 
-Without source:
-
-\begin{epigraph}
-  \randomtext
-\end{epigraph}
-
-The default width for the epigraph block is \autodoc:setting{epigraph.width}.
+The default width for the epigraph block is defined by the \autodoc:setting{epigraph.width}
+setting, which defaults to 60 percents of the curent line width.
 A \autodoc:parameter{width} option is also provided to override it on a single
-epigraph\footnote{Basically, all global settings are also available as
-command options (or reciprocally!), with the same name but the namespace left
-out. For the sake of brevity, we will therefore omit the namespace from this
-point onward.}.
+epigraph.
 It may be set to a relative value, e.g. 80 percents the current line
 width:
 
@@ -215,73 +186,26 @@ Or, pretty obviously, with a fixed value, e.g. 8cm.
   \randomtext
 \end{epigraph}
 
-The vertical skips are controlled by \autodoc:parameter{beforeskipamount},
-\autodoc:parameter{afterskipamount}, \autodoc:parameter{sourceskipamount}. The latter is
-only applied if there is a source specified and the epigraph doesn’t
-show a rule (see further below).
-
-In the following example, the two first options are set to
-0.5cm and the source skip is set to 0.
-
-\begin[beforeskipamount=0.5cm, afterskipamount=0.5cm, sourceskipamount=0]{epigraph}
-  \randomtext
-\source{\randomsource}
-\end{epigraph}
-
 By default, paragraph indentation is inherited from the document. It can be tuned
-with \autodoc:parameter{parindent}, e.g. 1em.
+with the \autodoc:parameter{parindent} option. For instance, we set it to 0 below.
 
-\begin[parindent=1em]{epigraph}
-  \randomtext
-
+\begin[parindent=0em]{epigraph}
   \randomtext
 \end{epigraph}
 
 A rule may be shown below the epigraph text (and above the source, if
 present — in that case, the vertical source skip amount does not
 apply). Its thickness is controlled with \autodoc:parameter{rule} being set
-to a non-null value, e.g. 0.4pt.
+to a non-null value, e.g. 0.4pt. The \autodoc:setting{epigraph.rule} may
+also be set to define the global default thickness.
 
 \begin[rule=0.4pt]{epigraph}
   \randomtext
   \source{\randomsource}
 \end{epigraph}
 
-Likewise, without source:
-
-\begin[rule=0.4pt]{epigraph}
-  \randomtext
-\end{epigraph}
-
-By default, the epigraph text is justified. This may be changed setting
-\autodoc:parameter{ragged} to true.
-
-The text is then ragged on the opposite side to the epigraph block,
-so on the left for a right-aligned block.
-
-\begin[ragged=true]{epigraph}
-  \randomtext
-  \source{\randomsource}
-\end{epigraph}
-
-It would be ragged on the right for a left-aligned epigraph block.
-
-\begin[align=left, ragged=true]{epigraph}
-  \randomtext
-  \source{\randomsource}
-\end{epigraph}
-
-Here, we introduced the \autodoc:parameter{align} option, set to \code{left}.
-All the settings previously mentioned also apply to left-aligned
-epigraphs, so we can of course tweak them at convenience.
-
-\begin[align=left, rule=0.4pt, parindent=0, width=45%lw]{epigraph}
-  \randomtext
-  \source{\randomsource}
-\end{epigraph}
-
-It is also possible to offset the epigraph from the side (left or right) it is attached to, with the
-\autodoc:parameter{margin} option, e.g. 0.5cm:
+It is also possible to offset the epigraph from the side (left or right, depending on
+styling) it is attached to, with the \autodoc:parameter{margin} option, e.g. 0.5cm:
 
 \begin[margin=0.5cm, rule=0.4pt]{epigraph}
   \randomtext
@@ -289,18 +213,8 @@ It is also possible to offset the epigraph from the side (left or right) it is a
 \end{epigraph}
 
 If you want to specify what styling the epigraph environment should use, you
-can redefine the \code{epigraph} style. By default it will be the same
-as the surrounding document, just smaller.
-The epigraph source is typeset in italic by default. It can be modified too,
-by redefining \code{epigraph-source}.
-
-As final notes, the epigraph source is intended to be short by nature, therefore
-no specific effort has been made to correctly handle sources longer than
-the epigraph block or even spanning on multiple lines.
-
-Before anyone asks, the alignment options for an epigraph are to the left or to the right
-of the frame only; notably, there is no intention to support centering. This author does not
-think centered epigraphs are typographically sound.
+can redefine the \code{epigraph} style (typically for global vertical skips,
+font size, etc.), and the \code{epigraph-text} and \code{epigraph-source} styles.
 
 For the curious-minded, it turns out that nested epigraphs do work somewhat as intended.
 Your mileage may vary depending on the combination of settings.
